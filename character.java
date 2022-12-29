10be8b200ea24c1aba0ea5b4e0a15b8942151050
yusramahivish <120807558+yusramahivish@users.noreply.github.com>
2022-12-29 18:38:54 +0530

    Create character.java

---
+++ character.java
@@ -0,0 +1,11 @@
+public class character {
+   public static void main(String[] args)
+    {
+        String a = "welcome";
+        System.out.println("Original String = " + a);
+        
+        int index = a.charAt(0);
+        System.out.println("The character at position 0 is " +
+            (char)index);
+    }
+}
